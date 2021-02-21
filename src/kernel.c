69f342a9b72227fc944661767fe5c620f4657f0d
hrshfr <hrshfr1@gmail.com>
2021-02-21 18:45:22 +0700

    add clear buffer

--- src/kernel.c
+++ src/kernel.c
@@ -1,11 +1,17 @@
 void handleInterrupt21 (int AX, int BX, int CX, int DX);
 void printString(char *string);
 // void readString(char *string);
-// void clear(char *buffer, int length); // Fungsi untuk mengisi buffer dengan 0
+void clear(char *buffer, int length); // Fungsi untuk mengisi buffer dengan 0
 
 int main() {
+  char* tes;
   makeInterrupt21();
-  printString("lorem ipsum dolor sit amet");
+  tes[0]='A';
+  tes[1]='B';
+  tes[2]='C';
+  tes[3]='\0';
+  printString(tes);
+  clear(tes,4);
   while (1);
 }
 void handleInterrupt21 (int AX, int BX, int CX, int DX){
@@ -25,6 +31,13 @@ void printString(char *string) {
   while (*string != '\0') {
     interrupt(0x10, 0xe * 256 + *string, 0, 0, 0);
     string++;
+  }
+}
+
+void clear(char *buffer, int length){
+  int i;
+  for(i=0;i<length;i++){
+    buffer[i]=0;
   }
 }
 
