5585c7dd05d1ccb76bc92b5e5969167de158a128
Govindabhakta <igovindabhakta@gmail.com>
2021-02-23 20:25:36 +0700

    Added read

--- src/kernel.c
+++ src/kernel.c
@@ -1,29 +1,31 @@
 void handleInterrupt21 (int AX, int BX, int CX, int DX);
 void printString(char *string);
-// void readString(char *string);
+void readString(char *string);
 void clear(char *buffer, int length); // Fungsi untuk mengisi buffer dengan 0
 
 int main() {
-  char* tes;
+  char tes[16];
   makeInterrupt21();
-  tes[0]='A';
-  tes[1]='B';
-  tes[2]='C';
-  tes[3]='\0';
+  readString(tes);
+  // tes[0]='A';
+  // tes[1]='B';
+  // tes[2]='C';
+  // tes[3]='\0';
   printString(tes);
-  clear(tes,4);
+  clear(tes,16);
   while (1);
 }
+
 void handleInterrupt21 (int AX, int BX, int CX, int DX){
   switch (AX) {
     case 0x0:
-    printString(BX);
-    break;
-  // case 0x1:
-  //   readString(BX);
-  //   break;
-  default:
-    printString("Invalid interrupt");
+      printString(BX);
+      break;
+    case 0x1:
+      readString(BX);
+      break;
+    default:
+      printString("Invalid interrupt");
   }
 }
 
@@ -31,6 +33,24 @@ void printString(char *string) {
   while (*string != '\0') {
     interrupt(0x10, 0xe * 256 + *string, 0, 0, 0);
     string++;
+  }
+}
+
+void readString(char *string)
+{
+  int i = 0;
+  while(i < 16)
+  {
+    *string = interrupt(0x16, 0, 0, 0, 0);
+    
+    if (*string != '\r')
+    {
+      string++;
+      i++;
+    } else {
+      *string = '\0';
+      i = 16;
+    }
   }
 }
 
