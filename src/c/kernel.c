1b8e10ede16f65cb57c7ee0589cf7425c238ceb1
jerwansyah <blcktmyie123@gmail.com>
2021-03-16 20:42:40 +0700

    fixed parsePath and isPathValid

--- src/c/kernel.c
+++ src/c/kernel.c
@@ -39,8 +39,8 @@ int main() {
   printString(test);
   printString("\r\n");
 
-  strncpy(buffer, test, 16);
-  writeFile(buffer, "./test.txt", 1, 0xFF);
+  // strncpy(buffer, test, 16);
+  // writeFile(test, "root", 1, 0xFF);
   clear(test,16);
 
   while (1);
@@ -189,12 +189,13 @@ char *parsePath(char *path, char parentIndex) {
         j++;
       }
 
-      if (j > FILE_NAME_LENGTH) {
+      if (j-1 > FILE_NAME_LENGTH) {
         printString("Path tidak valid!\r\n");
-        return -1;
+        pathDir[0] = -1;
+        return pathDir;
       }
 
-      strncpy(pathDir[k * FILE_ENTRY_LENGTH + 2], path[j], j);
+      strncpy(pathDir[k * FILE_ENTRY_LENGTH + 2], path[i], j);
       k++;
     } else {
       i++;
@@ -212,11 +213,12 @@ int isPathValid(char *path, char *parentIndex, char *dirBuffer) {
   int i;
 
   strncpy(dir, parsePath(path, parentIndex), FILE_ENTRY_TOTAL * FILE_NAME_LENGTH);
+  if (dir[0] == -1) { return 0; }
   currentParent = parentIndex;
 
   for (i = 0; i < FILE_ENTRY_TOTAL && dir[i * FILE_ENTRY_LENGTH + 2] != 0x00; i++) {
     clear(name, FILE_NAME_LENGTH);
-    if (strncmp(dir[i * FILE_ENTRY_LENGTH], "..", 2) == 0) {
+    if (strncmp(dir[i * FILE_ENTRY_LENGTH + 2], "..", 2) == 0) {
       if (currentParent == 0xFF) {
         return 0;
       } else {
@@ -227,8 +229,9 @@ int isPathValid(char *path, char *parentIndex, char *dirBuffer) {
       continue;
     }
 
-    strncpy(name, dir[i + 2], FILE_NAME_LENGTH);
+    strncpy(name, dir[i * FILE_ENTRY_LENGTH + 2], FILE_NAME_LENGTH);
     index = getFileIdx(name, currentParent, dirBuffer);
+    if (index == -1) { return 0; }
     currentParent = dirBuffer[index * FILE_ENTRY_LENGTH];
   }
 
